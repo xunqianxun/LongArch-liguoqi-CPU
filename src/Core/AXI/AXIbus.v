8dad3fa4bf1898becd42094d3e487f47fb1d3f4e
qianxunyu <779163410@qq.com>
2024-05-02 18:50:21 +0800

    Axi and cache design

--- src/Core/AXI/AXIbus.v
+++ src/Core/AXI/AXIbus.v
@@ -19,12 +19,12 @@ module AXIbus (
     input       wire                       DcaWriteAble,
     output      wire                       DWshankhand , //与uncache 共用
     inout       wire                       DWriteUncache,
+    input       wire     [3:0]             UncacheStrb , //uncache 时只可能是len为1的时候
     input       wire     [`InstAddrBus]    DcaWriteAddr,
-    input       wire     [256:0]           DcaWDate    ,
+    input       wire     [255:0]           DcaWDate    ,
     input       wire                       DcaReadAble ,
     output      wire                       DRshankhand ,
     input       wire                       DUnacheRead ,
-    input       wire     [2:0]             DcaMSHRPtr
     input       wire     [`InstAddrBus]    DcaReadAddr ,
     // read request
     output      wire     [ 3:0]            Arid        ,
@@ -90,14 +90,14 @@ module AXIbus (
 
     assign  Arvalid = (IcaReadAble | IUncacheRead | DcaReadAble | DUnacheRead) ;
     assign  Araddr  = (IcaReadAble | IUncacheRead) ? IcaReadAddr : 
-                      (DcaReadAble | DUnacheRead)  ? DcaWriteAddr: 32'd0 ;
+                      (DcaReadAble | DUnacheRead)  ? DcaReadAddr: 32'd0 ;
 
     assign  Arlen   = IUncacheRead ? 8'd1 :
                       IcaReadAble  ? 8'd8 : 
                       DUnacheRead  ? 8'd1 : 
                       DcaReadAble  ? 8'd4 : 8'd0 ;
 
-    assign [1:0] AlowSIgn = (IcaReadAble | IUncacheRead) ? 2'b01 :
+    wire  [1:0] AlowSIgn =  (IcaReadAble | IUncacheRead) ? 2'b01 :
                             (DcaReadAble | DUnacheRead)  ? 2'b10 : 2'b00 ;
 
     assign  Arid    = 4'd1   ; 
@@ -118,7 +118,7 @@ module AXIbus (
         else begin
             if(IRshankhand | DRshankhand)
                 RreadyTemp <= `AbleValue ;
-            if(Rlast & Rresp == 2'b00)
+            if(Rlast & (Rresp == 2'b00) & (Rid == 4'd1))
                 RreadyTemp <= `EnableValue ;
         end 
     end
@@ -126,35 +126,44 @@ module AXIbus (
     assign Rready = RreadyTemp | IRshankhand | DRshankhand ;
 
     integer i ;
-    reg [511:0] ReadDateTemp ;
+    reg [31:0] ReadDateTemp[1:16] ;
     reg [5:0]   ReadCont     ;
     always @(posedge Clk) begin
         if(!Rest) begin
-            ReadDateTemp <= 512'd0 ;
-            ReadCont     <= 6'd0   ;
+            for (i =0 ;i<16 ;i=i+1 ) begin
+                ReadDateTemp[i] <= 32'd0 ;
+            end
+            ReadCont     <= 6'd1   ;
         end 
         else begin
             if(Rready) begin
                 if(Rvalid) begin
-                    ReadDateTemp[ReadCont : ReadCont+32] <= Rdata ;
+                    ReadDateTemp[ReadCont[4:0]] <= Rdata ;
                     ReadCont <= ReadCont + 1 ;
                 end
                 else begin
-                    ReadDateTemp <= ReadDateTemp ;
+                    for (i =0 ;i<=16 ;i=i+1 ) begin
+                        ReadDateTemp[i] <= ReadDateTemp[i] ;
+                    end
                     ReadCont <= ReadCont ;
                 end
             end
             else begin
-                ReadDateTemp <= 512'd0 ;
-                ReadCont     <= 6'd0   ;
+                for (i =0 ;i<16 ;i=i+1 ) begin
+                    ReadDateTemp[i] <= 32'd0 ;
+                end
+                ReadCont     <= 6'd1   ;
             end     
         end 
     end 
 
-    assign ReadBackAble = (Rlast & Rresp == 2'b00) ;
-    assign ReadBackDate = ReadDateTemp ;
+    assign ReadBackAble = (Rlast & (Rresp == 2'b00) & (Rid == 4'd1)) ;
+    assign ReadBackDate = {ReadDateTemp[16],ReadDateTemp[15],ReadDateTemp[14],ReadDateTemp[13],
+                           ReadDateTemp[12],ReadDateTemp[11],ReadDateTemp[10],ReadDateTemp[9],
+                           ReadDateTemp[8],ReadDateTemp[7],ReadDateTemp[6],ReadDateTemp[5],
+                           ReadDateTemp[4],ReadDateTemp[3],ReadDateTemp[2],ReadDateTemp[1]} ;
 
-    assign RCacReadfree = (ReadState == IDLE ); //cache中发射数据需要打一个拍，避免组合逻辑环 ;
+    assign CacReadfree = (ReadState == IDLE ); //cache中发射数据需要打一个拍，避免组合逻辑环 ;
 
     assign NextReadState = Arvalid ? RADDRC : 
                            Rready  ? RDATEC : IDLE ;
@@ -175,22 +184,31 @@ module AXIbus (
     assign Awaddr  = DcaWriteAddr ;
     assign Awlen   = DcaWriteAble  ? 8'd4 : 
                      DWriteUncache ? 8'd1 : 8'd0 ;
+    assign Awid    = 4'd1   ;
     assign Awsize  = 3'b010 ;
     assign Awburst = 2'b01  ;
     assign Awlock  = 2'b0   ;
     assign Awcache = 4'b0   ;
     assign Awprot  = 3'b0   ;
 
-    reg [7:0]  WriteLenTemp ;
+    reg [7:0]   WriteLenTemp ;
+    reg [3:0]   WriteStrbTemp;
+    reg [255:0] WriteDateTemp;
     always @(posedge Clk) begin
         if(!Rest) begin
             WriteLenTemp <= 8'd0 ;
+            WriteStrbTemp <= 4'd0 ;
+            WriteDateTemp<= 256'd0 ;
         end
         else if(Awvalid) begin
             WriteLenTemp <= Awlen ;
+            WriteStrbTemp <= UncacheStrb ;
+            WriteDateTemp<= DcaWDate ;
         end
         else if(Wlast) begin
             WriteLenTemp <= 8'd0 ;
+            WriteStrbTemp <= 4'd0 ;
+            WriteDateTemp<= 256'd0 ;
         end
     end
 
@@ -198,34 +216,80 @@ module AXIbus (
 
     reg      WvalidTemp ; //用于标识处于写通道传输状态
     always @(posedge Clk) begin
-        if(!Rest) begin
+        if(!Rest) 
             WvalidTemp <= `EnableValue ;
-        end
-        else begin
-            
-        end
+        else if(DWshankhand) 
+            WvalidTemp <= `AbleValue ;
+        else if(Wlast) 
+            WvalidTemp <= `EnableValue ;
     end
 
-    reg         RegWvalid ;
+    reg    BReadyTemp ;
+    always @(posedge Clk) begin
+        if(!Rest)
+           BReadyTemp <=  `EnableValue ;
+        else if(DWshankhand) 
+            BReadyTemp <= `AbleValue ;
+        else if((Bid ==4'd1) & (Bresp == 2'd0) & (Bvalid))
+            BReadyTemp <=  `EnableValue ;
+    end
+
+    assign Bready = BReadyTemp ;
+
+    assign Wvalid = WvalidTemp ;
+
+    wire [31:0] WriteDAteDiv [1:8] ;
+    assign WriteDAteDiv[1] = WriteDateTemp[31:0] ;
+    assign WriteDAteDiv[2] = WriteDateTemp[63:32] ;
+    assign WriteDAteDiv[3] = WriteDateTemp[95:64] ;
+    assign WriteDAteDiv[4] = WriteDateTemp[127:96] ;
+    assign WriteDAteDiv[5] = WriteDateTemp[159:128] ;
+    assign WriteDAteDiv[6] = WriteDateTemp[191:160] ;
+    assign WriteDAteDiv[7] = WriteDateTemp[223:192] ;
+    assign WriteDAteDiv[8] = WriteDateTemp[255:224] ;
+
+
     reg [31:0]  RegWdate  ;
+    reg         RegWlast  ;
+    reg [7:0]   WriteCont ;
     always @(posedge Clk) begin
         if(!Rest) begin
-            RegWvalid <= `EnableValue ;
+            RegWlast  <= `EnableValue ;
             RegWdate  <= 32'd0 ;
+            WriteCont <= 8'd1  ;
         end 
         else begin
-            if()
+            if(WvalidTemp) begin
+                if(Wready & (WriteCont <= WriteLenTemp)) begin
+                    RegWlast <= ((WriteLenTemp-WriteCont) == 1) ;
+                    RegWdate <= WriteDAteDiv[WriteCont] ;
+                    WriteCont <= WriteCont+1 ; 
+                end
+                else begin
+                    RegWlast <= `EnableValue ;
+                    RegWdate <= 32'd0 ;
+                    WriteCont <= WriteCont ; 
+                end
+            end
+            else begin
+                RegWlast  <= `EnableValue ;
+                RegWdate  <= 32'd0 ;
+                WriteCont <= 8'd1  ;
+            end
         end
     end
 
-    assign NextWriteState = Awvalid ? WADDRC :
-                            (WriteState == WADDRC) ? WDATEC : 
+    assign NextWriteState = Awvalid    ? WADDRC :
+                            WvalidTemp ? WDATEC : 
+                            BReadyTemp ? WRESPC : IDLE ;
 
+    assign WritBAckAble = ((Bid ==4'd1) & (Bresp == 2'd0) & (Bvalid)) ;
+    assign CacWritefree = (WriteState == IDLE) ;
 
-
-
-
-
+    assign Wid      = 4'd1 ;
+    assign Wdata    = RegWdate ;
+    assign Wstrb    = (WriteLenTemp == 8'd1) ? WriteStrbTemp : 4'b1111  ;
+    assign Wlast    = RegWlast ;
 
 
 endmodule
--- src/Core/ICahce/ICacheStage2.v
+++ src/Core/ICahce/ICacheStage2.v
@@ -5,7 +5,7 @@ module ICacheStage2 (
     input       wire                                     Clk           ,
     input       wire                                     Rest          ,
     //for cache ctrl 
-    input       wire                                     IcacheStop    ,
+    //input       wire                                     IcacheStop    ,
     input       wire                                     ICacheFlash   ,
     output      wire                                     ICacheReq     ,
     output      wire                                     BpuReq        ,
@@ -16,28 +16,36 @@ module ICacheStage2 (
     input       wire       [`InstAddrBus]                InPhysicalAddr,         
     //from stage1 
     input       wire                                     InStage1Able  ,
-    input       wire       [7:0]                         In1Offset     ,
-    input       wire       [255:0]                       In1Way1Date   ,
+    input       wire       [`InstAddrBus]                InStage1Pc    ,
+    input       wire       [5:0]                         In1Offset     ,
+    input       wire       [511:0]                       In1Way1Date   ,
     input       wire       [19:0]                        In1Way1Tag    ,
-    input       wire       [255:0]                       In1Way2Date   ,
+    input       wire       [511:0]                       In1Way2Date   ,
     input       wire       [19:0]                        In1Way2Tag    ,
-    input       wire       [255:0]                       In1Way3Date   ,
+    input       wire       [511:0]                       In1Way3Date   ,
     input       wire       [19:0]                        In1Way3Tag    ,
-    input       wire       [255:0]                       In1Way4Date   ,
+    input       wire       [511:0]                       In1Way4Date   ,
     input       wire       [19:0]                        In1Way4Tag    ,  
     //to stage1 
     output      wire                                     OutHitAble    ,
-    output      wire       [3:0]                         OutHitIndex   ,
+    output      wire       [5:0]                         OutHitIndex   ,
     output      wire                                     OutHitWay1    ,
     output      wire                                     OutHitWay2    ,
     output      wire                                     OutHitWay3    ,
     output      wire                                     OutHitWay4    ,
     output      wire                                     OutNewAble    ,
-    output      wire       [3:0]                         OutNewIndex   ,
+    output      wire       [5:0]                         OutNewIndex   ,
     output      wire       [19:0]                        OutNewTag     ,
-    output      wire       [255:0]                       OutNewDate    ,
+    output      wire       [511:0]                       OutNewDate    ,
+    //for axi
+    output      wire                                     IcaReadAble   ,
+    input       wire                                     IRshankhand   ,
+    output      wire                                     IUncacheRead  ,
+    output      wire     [`InstAddrBus]                  IcaReadAddr   ,
+    input       wire                                     CacReadfree   ,
+    input       wire                                     ReadBackAble  ,
+    input       wire     [511:0]                         ReadBackDate  ,
     //to PreDecode 
-    //output      wire       [2:0]                         OutType       ,
     output      wire       [255:0]                       OutPcIvt      ,
     output      wire       [7:0]                         OutInstIvt    ,
     output      wire       [255:0]                       OutDate 
@@ -56,9 +64,50 @@ module ICacheStage2 (
     assign {WayHitAble,WayHitDate} = (~HitWay1 & ~HitWay2 & ~HitWay3 & HitWay4) ? {`AbleValue,In1Way1Date} : 
                                      (~HitWay1 & ~HitWay2 & HitWay3 & ~HitWay4) ? {`AbleValue,In1Way2Date} : 
                                      (~HitWay1 & HitWay2 & ~HitWay3 & ~HitWay4) ? {`AbleValue,In1Way3Date} : 
-                                     (HitWay1 & ~HitWay2 & ~HitWay3 & ~HitWay4) ? {`AbleValue,In1Way4Date} : {`EnableValue, 256'd0} ;
+                                     (HitWay1 & ~HitWay2 & ~HitWay3 & ~HitWay4) ? {`AbleValue,In1Way4Date} : {`EnableValue, 512'd0} ;
 
-    wire [255:0] OutDate = In1Offset == 8'd0 
+    wire [511:0] FInalOutDate ;
+
+    wire [255:0] OutDate = {256{(In1Offset == {4'd0,2'd0})}} & FInalOutDate[255:0]  |
+                           {256{(In1Offset == {4'd1,2'd0})}} & FInalOutDate[287:32] |
+                           {256{(In1Offset == {4'd2,2'd0})}} & FInalOutDate[319:64] |
+                           {256{(In1Offset == {4'd3,2'd0})}} & FInalOutDate[351:96] | 
+                           {256{(In1Offset == {4'd4,2'd0})}} & FInalOutDate[383:128] |
+                           {256{(In1Offset == {4'd5,2'd0})}} & FInalOutDate[415:160] |
+                           {256{(In1Offset == {4'd6,2'd0})}} & FInalOutDate[447:192] |
+                           {256{(In1Offset == {4'd7,2'd0})}} & FInalOutDate[479:224] |
+                           {256{(In1Offset == {4'd8,2'd0})}} & FInalOutDate[511:256] |
+                           {256{(In1Offset == {4'd9,2'd0})}}  & {32'd0,FInalOutDate[511:288]} |
+                           {256{(In1Offset == {4'd10,2'd0})}} & {64'd0,FInalOutDate[511:320]} |
+                           {256{(In1Offset == {4'd11,2'd0})}} & {96'd0,FInalOutDate[511:352]} |
+                           {256{(In1Offset == {4'd12,2'd0})}} & {128'd0,FInalOutDate[511:384]} |
+                           {256{(In1Offset == {4'd13,2'd0})}} & {160'd0,FInalOutDate[511:416]} |
+                           {256{(In1Offset == {4'd14,2'd0})}} & {192'd0,FInalOutDate[511:448]} |
+                           {256{(In1Offset == {4'd15,2'd0})}} & {224'd0,FInalOutDate[511:480]} ;
+
+                           
+    wire [31:0]  FInalPc    ;
+    wire [255:0] OutPcIvt = {FInalPc+28,FInalPc+24,FInalPc+20,FInalPc+16,
+                             FInalPc+12,FInalPc+8,FInalPc+4,FInalPc}
+
+    reg [37:0] MSHR[0:7] ;
+
+    integer i ;
+    always @(posedge Clk) begin
+        if(!Rest) begin
+            for (i =0 ;i<8 ;i=i+1 ) begin
+                MSHR[i] <= 38'd0 ;
+            end  
+        end
+        else begin
+            if(ICacheFlash) 
+                for (i =0 ;i<8 ;i=i+1 ) begin
+                    MSHR[i] <= 38'd0 ;
+                end
+            if()
+        end
+    end
+
     
 
 

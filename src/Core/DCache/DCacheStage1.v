7eb984e10f5334a095ab2c75ad218530ec977f30
xunqianxun <779163410@qq.com>
2024-05-05 19:11:39 +0800

    ICache and DCache design

--- src/Core/DCache/DCacheStage1.v
+++ src/Core/DCache/DCacheStage1.v
@@ -4,7 +4,19 @@
 module DCacheStage1 (
     input        wire                                 Clk          ,
     input        wire                                 Rest         ,
-    //
+    // for ctrl 
+    input        wire                                 DcacheStop   ,
+    input        wire                                 DcacheFlash  ,
+    output       wire                                 DcacheReq    ,
+    //from AGU1
+    input        wire                                 LoadAble     ,
+    output       wire                                 LoadReq      ,
+    input        wire     [`InstAddrBus]              LoadPhyAddr  ,
+    //from AGU2
+    input        wire                                 StoreAble    ,
+    output       wire                                 StoreReq     ,
+    input        wire     [`InstAddrBus]              StorePhyAddr ,
+    
 );
     
 endmodule
